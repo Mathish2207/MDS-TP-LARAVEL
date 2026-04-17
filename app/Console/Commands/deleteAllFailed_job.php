dfaf74e8986b40efaa655c22046b93e558c3d2c1
Mathis H <mathish2207@gmail.com>
2026-04-17 09:33:19 +0200

    feat: Add command to delete all failed jobs from the database

---
+++ app/Console/Commands/deleteAllFailed_job.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+
+#[Signature('scheduler:delete-all-failed-jobs')]
+#[Description('Delete all failed jobs')]
+class deleteAllFailed_job extends Command
+{
+    
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $failedJobs = \DB::table('failed_jobs')->get();
+
+        if ($failedJobs->isEmpty()) {
+            $this->info('No failed jobs found.');
+            return;
+        }
+
+        \DB::table('failed_jobs')->delete();
+
+        $this->info('All failed jobs have been deleted successfully.');
+    }
+}
